4d7945726719d94b3e64e3630bd1434f3acabd99
ASTRO000000 <153936139+ASTRO000000@users.noreply.github.com>
2023-12-15 18:21:54 +0200

    Add files via upload

---
+++ access/values.js
@@ -0,0 +1,11 @@
+// Активировать документы (true - включены, false - отключены)
+var isRightsEnabled = true; // Права
+var isCovidCertificateEnabled = true; // COVID-сертификат
+
+// Общее
+var birthdate = "24.02.2004"; // Дата рождения
+var fullname = "Кондратюк Олександр Романович" // ФИО
+var name = "Олександр" // Имя
+
+// Паспорт
+var passport_id = "739275497" // Номер паспорта
